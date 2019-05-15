ed4276e5d668688cfe68537c5306c7eb020b3594
lz <liuze@spacetech.com.cn>
2019-05-15 15:45:31 +0800

    123

--- src/entity/Cell.java
+++ src/entity/Cell.java
@@ -4,7 +4,7 @@ import custom.event.GameOverEvent;
 
 public abstract class Cell {
     private Integer x;
-    private Integer y;
+	private Integer y;// 1
     private String content;
     private Event event;
 
